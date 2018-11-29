4bf1e228e263d15788bfdfc0e1e37fa10a24491c
LARABA MOHAMMED SEDDIK <mslaraba@gmail.com>
2018-11-29 20:51:56 +0100

    Removed Demos

---
+++ DemoReport/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿/************************************************************************
+ * Copyright: Hans Wolff
+ *
+ * License:  This software abides by the LGPL license terms. For further
+ *           licensing information please see the top level LICENSE.txt 
+ *           file found in the root directory of OptimIT Reports.
+ *
+ * Author:   Hans Wolff
+ *
+ ************************************************************************/
+
+using System.Windows;
+
+namespace DemoReport
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
